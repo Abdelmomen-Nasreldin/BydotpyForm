75c709cacb73e20889de9eae59328ef0eca53c2f
Abdelmomen Nasreldin <abdelmomennasreldin@gmail.com>
2023-10-06 23:20:26 +0300

    add any alternatib=ve to 2 and 3 steps

--- src/components/StepThreeForm/StepThreeForm.tsx
+++ src/components/StepThreeForm/StepThreeForm.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const StepThreeForm = () => {
-  return (
-    <div>StepThreeForm</div>
-  )
-}
+const StepThreeForm =({next, back}: {next: ()=>void, back: ()=>void} ) => {
+    return (
+      <form className='h-[50vh]  text-white'>
+        <button className='px-5 py-3 m-2 rounded w-full bg-primary' type="button" onClick={back}>Previous</button>
+        <button className='px-5 py-3 m-2 rounded w-full bg-primary' type="button" onClick={next}>Next</button>
+      </form>
+    )
+  }
 
 export default StepThreeForm
--- src/components/StepTwoForm/StepTwoForm.tsx
+++ src/components/StepTwoForm/StepTwoForm.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const StepTwoForm = () => {
+const StepTwoForm =({next, back}: {next: ()=>void, back: ()=>void} ) => {
   return (
-    <div>StepTwoForm</div>
+    <form className='h-[50vh]  text-white'>
+      <button className='px-5 py-3 m-2 rounded w-full bg-primary' type="button" onClick={back}>Previous</button>
+      <button className='px-5 py-3 m-2 rounded w-full bg-primary' type="button" onClick={next}>Next</button>
+    </form>
   )
 }
 
